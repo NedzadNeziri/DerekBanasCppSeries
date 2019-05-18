e06179a784b2d526494347711ea8da000c8c74f8
NedzadNeziri <nedzad.neziri@outlook.com>
2019-05-18 10:27:39 +0200

    refactoring

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -2,17 +2,16 @@
 #include <string>
 #include <iomanip>
 
-//using namespace std; // step 2. Comment out this!
+using namespace std;
 
-void cout(const char* input)
-{
-	std::cout << input;
-}
 int main()
 {
-	// Compiler doesn't know what function to call
-	// do we call std::cout or cout?
-	cout("THIS IS THE TEST");
-	// step 2. now I will comment out the "using namespace std"
-
+	std::string userInput;
+	printf("Enter miles, and I will convert them to kilometers\n");
+	std::getline(std::cin, userInput);
+	double stringToDouble = std::stod(userInput);  // function that will convert string to double
+	printf("%d miles equals %5.4f kilometers\n", int(stringToDouble), (stringToDouble * 1.60934));	//	C style code
+	std::cout << int(stringToDouble) << " miles equals " << std::setprecision(5) << (stringToDouble * 1.60934) << " kilometers\n";	//	C++ style code, including iomanip
+	return 0;
+	std::cin.get();
 }
